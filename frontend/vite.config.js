629db018fb111befec256a8835cbe79a8b1c292c
VenkataramanTB <98270783+venkataramanTB@users.noreply.github.com>
2026-07-01 01:51:57 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,13 +6,14 @@ export default defineConfig(({ mode }) => {
 
   return {
     base: './',
-
     plugins: [react()],
 
     server: {
       host: '0.0.0.0',
       port: Number(process.env.PORT) || 8080,
-      strictPort: true,
+
+      // 👇 Add this
+      allowedHosts: true,
 
       cors: true,
 
